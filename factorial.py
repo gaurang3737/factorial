5519273d5e336af281b37446f1ee462665322a80
gaurang3737 <gaurangpatel3737@gmail.com>
2020-04-08 15:34:06 -0400

    Bug fixed factorial n*1
    Fixed problem with last line of factorial(n) function which now return n * factorial(n-1)
    
    Added New Test Case

--- factorial.py
+++ factorial.py
@@ -5,11 +5,13 @@ def factorial(n):
     if n == 1:
         return 1
     else:
-        return n * 1
+        return n * factorial(n-1)
 
 class FactorialTest(unittest.TestCase):
     def test_factorial(self) -> None:
         self.assertEqual(factorial(1),1)
+        self.assertEqual(factorial(2),2)
+        self.assertEqual(factorial(5),120)
 
 if __name__ == "__main__":
     unittest.main(exit = False)
